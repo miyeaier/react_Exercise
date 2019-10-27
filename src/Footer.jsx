4363395110cd12a62122aab9a71059c1cda4ed2d
miyesier <miyesier@gmail.com>
2019-10-27 21:21:45 +0100

    Adds Footer and Header

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+     <h1> Made with MiyesierKaerman {React.version}</h1>
+    </footer>
+  )
+}
+export default Footer
+
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <nav>
+    <h1>BrainsSvamp</h1>
+    </nav>
+  )
+}
+export default Header 
